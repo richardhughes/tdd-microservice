420d97adeb91c0c0c6f1d7997325fe4477371a35
Richie Hughes <eeuc40@hotmail.com>
2017-09-06 20:53:25 +0100

    Add first test

---
+++ tests/Acceptance/Http/Controllers/AuthenticationControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Acceptance\Http\Controllers;
+
+use TestCase;
+
+class AuthenticationControllerTest extends TestCase
+{
+    public function testWorks()
+    {
+        $this->assertTrue(true);
+    }
+}
