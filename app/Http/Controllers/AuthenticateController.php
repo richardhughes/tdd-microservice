50ddcce251f77b5100605fe90b92660d7c8f6ddf
Richie Hughes <eeuc40@hotmail.com>
2017-09-06 21:09:42 +0100

    Add authenticate endpoint

---
+++ app/Http/Controllers/AuthenticateController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: richie
+ * Date: 06/09/17
+ * Time: 21:02
+ */
+
+namespace App\Http\Controllers;
+
+use Laravel\Lumen\Routing\Controller as BaseController;
+
+class AuthenticateController extends BaseController
+{
+
+    public function index()
+    {
+        return response()->json([
+            'token' => 'this-is-a-token'
+        ]);
+    }
+
+}
